a7f1ecefd1a28d7565b3b1687c1395a056bb125a
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-04-04 23:24:30 +0530

    feat signup page mockup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -15,18 +15,18 @@ export default function SignUpPage() {
       <h1 className="text-center text-3xl font-bold">SignUp Page</h1>
       <div className="flex flex-col items-start gap-2 bg-white p-2 rounded-lg text-black">
         <label htmlFor="email" className="font-medium text-xl">
-          Email
+          Username :
         </label>
         <input
           className="w-full  p-1 border-black border border-solid rounded-lg"
-          type="email"
-          name="email"
-          id="email"
-          value={user.email}
+          type="username"
+          name="username"
+          id="username"
+          value={user.username}
           onChange={(e) => setUser({ ...user, username: e.target.value })}
         />
         <label htmlFor="email" className="font-medium text-xl">
-          Email
+          Email :
         </label>
         <input
           className="w-full  p-1 border-black border border-solid rounded-lg"
@@ -34,20 +34,29 @@ export default function SignUpPage() {
           name="email"
           id="email"
           value={user.email}
-          onChange={(e) => setUser({ ...user, username: e.target.value })}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
         />
         <label htmlFor="email" className="font-medium text-xl">
-          Email
+          Password :
         </label>
         <input
           className="w-full  p-1 border-black border border-solid rounded-lg"
-          type="email"
-          name="email"
-          id="email"
-          value={user.email}
-          onChange={(e) => setUser({ ...user, username: e.target.value })}
+          type="password"
+          name="password"
+          id="password"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
         />
+        <button
+          onClick={() => onSignUP()}
+          className="w-full p-1 rounded-lg bg-black text-white text-xl font-medium"
+        >
+          SignUp here
+        </button>
       </div>
+      <Link className="underline" href="/login">
+        Visit Login
+      </Link>
     </div>
   );
 }
